401ac5994572c7c95993e1bd463448d91406996c
ristokoort <ristokoort@yahoo.com>
2022-10-05 14:11:36 +0300

    Add custon LogAttribute to CuisineController.

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,27 @@ namespace OdeToFood.Controllers
 {
     public class CuisineController : Controller
     {
-        public IActionResult Search(string name="unknown")
+        // [Authorize]
+        public IActionResult Search(string name = "unknown")
         {
             var message = HtmlEncoder.Default.Encode(name);
-            
+
             return Content(message);
-            return File("/css/site.css", "text/css");
+            // return new EmptyResult();
+            //return File("/css/site.css", "text/css");
+            //return Json(HtmlEncoder.Default);
+            //return Redirect("https://www.tthk.ee");
+            //return RedirectToRoute("default",new {controller:"cuisine",
+            //action = "Second", count=5}); 
+            //return RedirectToAction(nameof(Second), new{count=5 });
+            //return View("/home/index");
+        }
+
+            public IActionResult Second(int count)
+        {
+            return Content($"Teine on siin!{count}");
+        }
+
         }
     }
-}
+
--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OdeToFood.Filters;
 using OdeToFood.Models;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
+    [Log]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
