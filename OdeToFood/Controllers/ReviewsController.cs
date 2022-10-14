fec88b0773653a4fa3f825e29d6ac187cb4e45d1
ristokoort <ristokoort@yahoo.com>
2022-10-14 11:31:31 +0300

    Add a review creation capability

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -28,8 +28,25 @@ namespace OdeToFood.Controllers
             {
                 return NotFound();
             }
-
             return View(restaurant);
+        }
+
+        [HttpGet]
+            public ActionResult Create(int restaurantId)
+            {
+                return View();
+            }
+        
+        [HttpPost]
+        public ActionResult Create(RestaurantReview review) 
+        {
+            if(ModelState.IsValid)
+            {
+                _context.RestaurantReviews.Add(review);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index), new { id = review.RestaurantId });
+            }
+            return View(review);
         }
     }
 }
