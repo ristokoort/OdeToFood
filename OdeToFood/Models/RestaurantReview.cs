53402b7b42488745bf043399e427d6f399d66cbc
ristokoort <ristokoort@yahoo.com>
2022-10-19 15:32:24 +0300

    Add a custom validation maxwords

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.Models
 {
+    //public class MaxWordSAttribute:ValidationAttribute
+    //{
+    //    private readonly int _maxWords;
+    //    public MaxWordSAttribute(int maxwords):base("{0} has too many words")
+    //    {
+    //        _maxWords = maxwords;
+    //    }
+    //    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    //    {
+    //        if (value!=null)
+    //        {
+    //            var valueAsString = value.ToString();
+    //            if(valueAsString.Split(' ').Length>_maxWords)
+    //            {
+    //                var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+    //                return new ValidationResult(errorMessage);
+    //            }
+    //        }
+    //        return ValidationResult.Success;
+    //    }
 
+    }
     public class RestaurantReview
     {
         public int Id { get; set; }
