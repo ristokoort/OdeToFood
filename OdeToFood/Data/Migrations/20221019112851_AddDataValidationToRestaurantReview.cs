3691b8ef5e7d4e7bbb3b97ad6ab17a18b1ec74fd
ristokoort <ristokoort@yahoo.com>
2022-10-19 14:47:20 +0300

    Add validation annotations

---
+++ OdeToFood/Data/Migrations/20221019112851_AddDataValidationToRestaurantReview.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OdeToFood.Data.Migrations
+{
+    public partial class AddDataValidationToRestaurantReview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewerName",
+                table: "RestaurantReviews",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "RestaurantReviews",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewerName",
+                table: "RestaurantReviews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1024)",
+                oldMaxLength: 1024,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "RestaurantReviews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1024)",
+                oldMaxLength: 1024);
+        }
+    }
+}
