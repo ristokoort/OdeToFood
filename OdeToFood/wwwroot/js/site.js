35c59ab179c23272bdd35edec0e0f1a6a5b07c29
ristokoort <ristokoort@yahoo.com>
2022-10-21 09:35:54 +0300

    Add an Async Search

--- OdeToFood/wwwroot/js/site.js
+++ OdeToFood/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    var ajaxFormSubmit = function () {
+        var $form = $(this);
+
+        var options = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        };
+
+        $.ajax(options).done(function (data) {
+            var $target = $($form.attr("data-otf-target"));
+            $target.replace(data);
+        });
+        return false;
+    };
+
+
+    $("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
+});
